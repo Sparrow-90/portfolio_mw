34483f020f74d09be8cb0de461fdc8d2fae262d2
Mateusz Wróbel <mw.animacja@gmail.com>
2023-12-11 17:42:53 +0100

    Get projects list from Github API

---
+++ src/main.js
@@ -0,0 +1,45 @@
+import smallStar from "./assets/small-star.svg";
+import demoIcon from "./assets/demo.svg";
+import githubIcon from "./assets/github-icon.svg";
+
+const projectsContainer = document.querySelector('.projects--js');
+const username = 'Sparrow-90';
+const direction = 'desc';
+
+fetch(`https://api.github.com/users/${username}/repos?direction=${direction}`)
+.then(response => response.json())
+.then(response => {
+    for (let repository of response) {
+        const {name, html_url, description, stargazers_count, topics, homepage} = repository;
+        let tags = ``;
+        for (topic of topics){
+            tags += `<li class=" bg-gray-400/10 py-1 px-2 text-gray-400 font-bold leading-none rounded text-sm">${topic}</li>`
+        }
+
+        const element = `<article class="rounded-myRound md:rounded-myRound-xl overflow-clip bg-gradient-to-br from-white/10 to-white/5">
+        <div class="h-11 p-4 bg-gradient-to-br from-white/10 to-white/5 flex gap-1.5 shadow-innerLight rounded-t-myRound md:rounded-t-myRound-xl border-b border-bg">
+            <span class="w-3 h-3 bg-bg block rounded-full opacity-50"></span>
+            <span class="w-3 h-3 bg-bg block rounded-full opacity-50"></span>
+            <span class="w-3 h-3 bg-bg block rounded-full opacity-50"></span>
+        </div>
+        <div class="p-5 md:p-6 lg:p-10">
+        <header class="flex gap-4 items-center mb-4">
+            <h3 class="text-2xl font-bold">${name}</h3>
+            <p class="flex gap-0.5 items-center bg-gray-400/10 py-1 px-2 text-gray-400 font-medium rounded leading-none"><img src="${smallStar}" alt="">${stargazers_count}</p>
+        </header>
+        <p class="text-gray-400 text-xl mb-4">${description}</p>
+        <ul class="flex gap-2 mb-10">
+            ${tags}
+        </ul>
+        <div class="flex flex-col gap-4 items-start md:flex-row">
+            <a class="text-accent bg-bg flex gap-3 font-bold py-4 px-3 items-center rounded-lg md:rounded-xl md:text-xl border-lightGray border-2" href="${homepage}" target="_blank" rel="noreferrer nofollow">
+                <img class="w-6 h-6" src="${demoIcon}" alt=""> View demo</a>
+            <a class="text-accent bg-bg flex gap-3 font-bold py-4 px-3 items-center rounded-lg md:rounded-xl md:text-xl border-lightGray border-2" href="${html_url}" target="_blank" rel="noreferrer nofollow">
+                <img class="w-6 h-6" src="${githubIcon}" alt="">Source code</a>
+        </div>
+        </div>
+        </article>`;
+        if (homepage) projectsContainer.insertAdjacentHTML('afterbegin', element);
+    }
+})
+
